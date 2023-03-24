ec732ad09a19c8441af4d63d9958b9ca0e6dfd35
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-03-24 10:20:47 -0700

    To create module files in JS and linked files are HTML

---
+++ index.js
@@ -0,0 +1,74 @@
+import { objects } from "./obj.js"
+
+let a = "I'm Web Developer ROHIT";
+
+let b = document.createElement('div')
+b.id = "main";
+
+document.body.appendChild(b);
+
+b.innerText = `User: ${a}`;
+
+let form = document.createElement("form");
+form.className = "form bg-secondary";
+form.method = "get";
+
+
+let f_name = document.createElement("h1")
+f_name.innerText = "Login Form"
+
+let userName = document.createElement("input");
+userName.type = "text";
+
+userName.id = "userName";
+userName.placeholder = "user Name";
+userName.required = true;
+
+let pass = document.createElement("input");
+pass.type = "password";
+pass.id = "password";
+pass.placeholder = "Your Password";
+pass.required = true;
+
+let subBtn = document.createElement("input");
+subBtn.type = "submit";
+subBtn.value = "SUBMIT";
+subBtn.id = "subBtn";
+subBtn.className = "bg-primary";
+
+document.body.appendChild(form);
+form.appendChild(f_name)
+form.appendChild(userName);
+form.appendChild(pass)
+form.appendChild(subBtn)
+
+$(document).ready(function () {
+    $("#subBtn").click(function (e) {
+        e.preventDefault();
+
+        var u_text = $("#userName").val();
+        var p_text = $("#password").val();
+
+
+
+
+        for (var i = 0; i < objects.length; i++) {
+            var obb = objects[i];
+            var user = obb.u;
+            var uPass = obb.p;
+            if (user == u_text)
+                break;
+        }
+        if (user == u_text && uPass == p_text) {
+            alert("login successfully");
+        } else if (user == u_text && uPass != p_text) {
+            alert("Your Password is Invalid")
+        } else {
+            alert("user not found");
+        }
+
+
+    });
+});
+
+
---
+++ obj.js
@@ -0,0 +1,26 @@
+export var objects = [
+{
+    u: "rohit",
+    p: "r1234"
+},
+{
+    u: "vedant",
+    p: "v1234"
+},
+{
+    u: "saurabh",
+    p: "s1234"
+},
+{
+    u: "akshay",
+    p: "a1234"
+},
+{
+    u: "vaibhav",
+    p: "v1234"
+},
+{
+    u: "roshan",
+    p: "r1234"
+}
+]
